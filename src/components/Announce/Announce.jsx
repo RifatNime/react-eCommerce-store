127311eed418e9f1f243b75793164766a3ac8f6f
RifatNime <mdrifatnime@gmail.com>
2022-08-01 17:10:40 +0600

    Announce ment add

---
+++ src/components/Announce/Announce.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+
+function Announce() {
+  return (
+    <div className='bg-[#8a4af3] font-bold text-white flex items-center justify-center'>
+        <h2>Hurry up 50% off Sale!</h2>
+        <CloseIcon/>
+    </div>
+  );
+}
+
+export default Announce;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Announce from '../../components/Announce/Announce'
+// import Asssssss from '../../components/Announce/as'
+
+
+function Home() {
+  return (
+    <div className=''>
+      <Announce/>
+      {/* <Asssssss/> */}
+    </div>
+  )
+}
+
+
+export default Home
