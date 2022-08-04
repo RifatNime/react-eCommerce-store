a1c2abc222fb696a854a3d68199197c39a484547
RifatNime <mdrifatnime@gmail.com>
2022-08-04 16:49:53 +0600

    banner part commit

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,13 +7,19 @@ function Slider() {
         <div className='parentDiv h-[540px] bg-white flex items-center justify-between'>
             {/* left arrow */}
             <div className={arrowStyle}>
-                <ArrowLeftOutlined style={{fontSize: '50px'}}/>
+                <ArrowLeftOutlined style={{ fontSize: '50px' }} />
             </div>
             {/* slider part */}
-            <div className="wrapper flex w-[100%] h-[500px]"></div>
+            <div className="wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px overflow-hidden relative">
+                <div className='slide flex items-center justify-center h-[100%]'>
+                    <div className="image">
+                        <img src="https://st3.depositphotos.com/7713882/17906/i/600/depositphotos_179068502-stock-photo-happy-woman-showing-good-big.jpg" alt="slide_image" />
+                    </div>
+                </div>
+            </div>
             {/* right arrow */}
             <div className={arrowStyle}>
-                <ArrowRightOutlined style={{fontSize: '50px'}}/>
+                <ArrowRightOutlined style={{ fontSize: '50px' }} />
             </div>
         </div>
     )
